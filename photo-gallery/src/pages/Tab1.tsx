07012bd1d2719c6a8c0aa5f0079460114152964e
William Taylor <wtaylor1984@gmail.com>
2024-12-10 22:46:25 -0800

    adding the tab 1 no photo yet

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,23 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import ExploreContainer from '../components/ExploreContainer';
+import React, {useState, useEffect} from 'react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  const [dataset, setDataSet] = useState([]);
+
+  const dataURL = "https://dev-cs55-fall2024-williamtaylor.pantheonsite.io/wp-json/twentytwentyone-child/v1/wk16";
+
+  useEffect( () => {
+    fetch(dataURL)
+    .then(response => response.json())
+    .then(data => setDataSet(data))
+  },[])
+
+  console.log(dataset);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Posts Info</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+    
+      {dataset.map((item, index) => (
+        <IonItem key={index}>
+          <IonLabel>
+            <h4>{item.post_title}</h4>
+            <p>Written by: {item.user_nicename} On: {item.post_date} <a href={item.guid} rel="noreferrer">Link</a></p>
+            
+          </IonLabel>
+        </IonItem>
+      ))}
+      <IonItem>
+
+      </IonItem>
     </IonPage>
   );
 };
